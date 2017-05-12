cc6c33e2b998e60dd08a736709fd499311907925
jules_griffin <llpjules@gmail.com>
2017-05-12 16:31:33 -0700

    Dictionaries.py file

---
+++ dictionaries.py
@@ -0,0 +1,30 @@
+Create a dictionary containing some information about yourself.
+ The keys should include name, age,
+  country of birth, favorite language.
+
+ myself={}
+name[nme]="My name is Jules"
+age[age]="My age is 37"
+country[ctry]="My country of birth is  The USA"
+favlang[lang]="My favorite language Spanish"
+
+
+There are two steps to this process, building a dictionary
+ and then gathering all the data from it.
+    for data in name:
+        print data
+    for val in in age.itervalues():
+        print values
+    for key.data in country.iteritems():
+        print key, "=", data
+    for key in capitals.iterkeys():
+        print key    
+
+
+
+
+  Write a function that can take in and print out
+   any dictionary keys and values.
+
+   for k, v in d.iteritems():
+       print k, d
